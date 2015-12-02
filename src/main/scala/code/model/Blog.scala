1c074e3d59f22f8c47492399d66683ca3c384fc4
Paul Murphy <paul.murphy@fairfaxmedia.com.au>
2015-12-03 06:28:28 +1100

    Adding support for handling dynamic requests to SiteMap

--- src/main/scala/code/model/Blog.scala
+++ src/main/scala/code/model/Blog.scala
@@ -1,21 +1,30 @@
 package code.model
 
+import net.liftweb.common.Box
+import net.liftweb.common.Full
+
 case class Blog(headline: String, displayPath: String, introText: String, body: String)
 
+/**
+ * TODO: The datasource should be mixed in for unit testing.
+ */
 object Blog {
   
-  def findAll:Option[List[Blog]] = 
-    Some(List(
+  private val blogs = List(
       Blog(
           "Continous Integration with codeship", 
-          "/continous-integration-with-codeshop", 
+          "continous-integration-with-codeshop", 
           "A how to on CI/CD with codeship for liftweb", 
           "A how to on CI/CD with codeship for liftweb"),
       Blog(
           "Property-based testing", 
-          "/property-based-testing--what-is-it?", 
+          "property-based-testing--what-is-it?", 
           "Property based testing frameworks run the same test over and over, with generated input. This is in contrast to example based testing, which is what we typicallly do when we write unit tests.", 
           "Property based testing frameworks run the same test over and over, with generated input. This is in contrast to example based testing, which is what we typicallly do when we write unit tests.")
-    ))
+    ) 
+  
+  def findAll:Box[List[Blog]] = Full(blogs)
+  
+  def findByDisplayPath(displayPath: String):Box[Blog] = blogs.find(x => x.displayPath == displayPath )
   
 }
